602d4200d818973141883a8a907f19bf8258ed87
joseluismanonrosario <74918414+joseluismanonrosario@users.noreply.github.com>
2021-05-11 00:20:34 -0400

    Proyecto de node express y hbs

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require ("express");
+const exphbs = require ("express-handlebars");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main-view",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+app.get("/", (req, res)=>{
+    res.render("index");
+});
+
+app.get("/about", (req, res)=>{
+  res.render("about");
+});
+
+app.get("/contact", (req, res)=>{
+  res.render("contact");
+});
+
+app.get("/works", (req, res)=>{
+  res.render("works");
+});
+
+app.get("/services", (req, res)=>{
+  res.render("services");
+})
+
+app.listen(3000, ()=>{
+
+    console.log("Server on port 3000");
+});
+
